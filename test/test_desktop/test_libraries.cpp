00a9827246ec69a1cf0888c7c498c17c8d3430bd
Thomas Schuster <twihno@gmail.com>
2021-06-19 00:35:36 +0200

    Implemented tests for pullupinput and relay

--- test/test_desktop/test_libraries.cpp
+++ test/test_desktop/test_libraries.cpp
@@ -2,6 +2,8 @@
 #include <ArduinoFake.h>
 #include <unity.h>
 
+#include "Constants.hpp"
+
 #include "Relay.hpp"
 #include "PullupInput.hpp"
 #include "LEDLighting.hpp"
@@ -30,15 +32,41 @@ void test_relay(void)
     When(Method(ArduinoFake(), digitalRead)).AlwaysReturn(1);
 
     Relay r = Relay(13, LOW);
+
+    // Initially a relay is required to be off
+    TEST_ASSERT_FALSE(r.isOn());
+
+    // Test if we can turn on the relay
+    r.on();
+    TEST_ASSERT_TRUE(r.isOn());
+
+    // Test if we can turn off the relay
+    r.off();
+    TEST_ASSERT_FALSE(r.isOn());
+
+    // Test if we can toggle the relay
+    r.toggle();
+    TEST_ASSERT_TRUE(r.isOn());
+    r.toggle();
+    TEST_ASSERT_FALSE(r.isOn());
+    r.toggle();
+    r.toggle();
+    TEST_ASSERT_FALSE(r.isOn());
 }
 
 void test_pullupinput(void)
 {
     When(Method(ArduinoFake(), pinMode)).AlwaysReturn();
     When(Method(ArduinoFake(), digitalWrite)).AlwaysReturn();
-    When(Method(ArduinoFake(), digitalRead)).AlwaysReturn(1);
+    When(Method(ArduinoFake(), digitalRead)).Return(1, 0, 1, 0);
 
     PullupInput pullinput = PullupInput(13);
+
+    TEST_ASSERT_FALSE(pullinput.isOn());
+    TEST_ASSERT_TRUE(pullinput.isOn());
+
+    TEST_ASSERT_TRUE(pullinput.isOff());
+    TEST_ASSERT_FALSE(pullinput.isOff());
 }
 
 void test_enclosurepower(void)
@@ -59,21 +87,21 @@ void test_enclosurepower(void)
     // Test if enclosurePower turns off after timeout has been reached and autoOff is enabled
     enclosurePower.enableAutoOff();
     enclosurePower.setOn(0);
-    enclosurePower.tick(100);
+    enclosurePower.tick(ENCLOSUREPOWER_TIMEOUT - 1);
     TEST_ASSERT_TRUE(enclosurePower.isOn());
-    enclosurePower.tick(10000);
+    enclosurePower.tick(ENCLOSUREPOWER_TIMEOUT);
     TEST_ASSERT_TRUE(enclosurePower.isOn());
-    enclosurePower.tick(10001);
+    enclosurePower.tick(ENCLOSUREPOWER_TIMEOUT + 1);
     TEST_ASSERT_FALSE(enclosurePower.isOn());
 
     // Test if enclosurePower stays on after timeout has been reached and autoOff is disabled
     enclosurePower.disableAutoOff();
     enclosurePower.setOn(0);
-    enclosurePower.tick(100);
+    enclosurePower.tick(ENCLOSUREPOWER_TIMEOUT - 1);
     TEST_ASSERT_TRUE(enclosurePower.isOn());
-    enclosurePower.tick(10000);
+    enclosurePower.tick(ENCLOSUREPOWER_TIMEOUT);
     TEST_ASSERT_TRUE(enclosurePower.isOn());
-    enclosurePower.tick(10001);
+    enclosurePower.tick(ENCLOSUREPOWER_TIMEOUT + 1);
     TEST_ASSERT_TRUE(enclosurePower.isOn());
 }
 
@@ -94,11 +122,11 @@ void test_ledlighting(void)
     
     // Test if ledLighting turns off after timeout has been reached
     ledLighting.setOn(0);
-    ledLighting.tick(100);
+    ledLighting.tick(LEDLIGHTING_TIMEOUT - 1);
     TEST_ASSERT_TRUE(ledLighting.isOn());
-    ledLighting.tick(10000);
+    ledLighting.tick(LEDLIGHTING_TIMEOUT);
     TEST_ASSERT_TRUE(ledLighting.isOn());
-    ledLighting.tick(10001);
+    ledLighting.tick(LEDLIGHTING_TIMEOUT + 1);
     TEST_ASSERT_FALSE(ledLighting.isOn());
 }
 
